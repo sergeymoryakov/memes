9f2a8175fff47a924bd7caa0742882685bb4bb82
Sergey Moryakov <sergey@moryakov.com>
2023-07-10 17:19:04 +0300

    set image selection

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+const baseUrl = "https://api.imgflip.com/get_memes";
+let memes = [];
+
 const imageSelectorNode = document.getElementById("imageSelector");
 const inputTextTopNode = document.getElementById("inputTextTop");
 const inputTextBottomNode = document.getElementById("inputTextBottom");
 const memeImgNode = document.getElementById("memeImg");
 
+init();
+
+imageSelectorNode.addEventListener("input", displayImg);
+
+function init() {
+    fetchMemes().then((res) => {
+        if (res.success === true) {
+            memes = res.data.memes;
+
+            console.log("memes received, meme[0]:");
+            console.log(memes[0].name);
+            console.log(memes[0].url);
+            console.log(memes[0].id);
+
+            renderOptions(memes);
+
+            console.log("options rendered");
+        } else {
+            console.log("API request failed");
+        }
+    });
+}
+
 function fetchMemes() {
-    const baseUrl = "https://api.imgflip.com";
-    return fetch(`${baseUrl}/get_memes`).then((data) => data.json());
+    return fetch(baseUrl).then((data) => data.json());
 }
 
 function renderOptions(memes) {
@@ -19,26 +44,25 @@ function renderOptions(memes) {
     imageSelectorNode.innerHTML = optionList;
 }
 
+function displayImg() {
+    console.log("imageSelectorNode.value = ", imageSelectorNode.value);
+    const memeId = getMemeId(memes, imageSelectorNode.value);
+    console.log("memeId = ", memeId);
+    renderImg(memes, memeId);
+}
+
+function getMemeId(memes, imgId) {
+    let findId = "";
+    for (let index = 0; index < memes.length; index++) {
+        if (memes[index].id === imgId) {
+            findId = index;
+            console.log("findId = ", findId);
+        }
+    }
+    return findId;
+}
+
 function renderImg(memes, id) {
     memeImgNode.src = memes[id].url;
     console.log(memeImgNode.src);
 }
-
-function init() {
-    fetchMemes().then((res) => {
-        if (res.success === true) {
-            const memes = res.data.memes;
-            // Check - for TBS only:
-            console.log(memes);
-            console.log(memes[0]);
-
-            renderImg(memes, 96);
-            renderOptions(memes);
-            console.log("init completed");
-        } else {
-            console.log("API request failed");
-        }
-    });
-}
-
-init();
